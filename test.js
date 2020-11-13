5a58b00be8e1a7b65d43d29fe13614300a3d57e2
luong cong duy <duylcong97@gmail.com>
2020-11-13 12:59:15 +0700

    Upload

--- test.js
+++ test.js
@@ -1,3 +1,26 @@
-var string = 'hello Word';
+
+
+const movie={
+    name:'Star War',
+    genre:'fiction',
+    description:'This is movie about space war....',
+    actor:'luke skywalker',
+    price:100000,
+    duration: 100,
+    score:8,
+    releaseYear: 1997,
+    viewCount:420000,
+}
 console.log(string)
 
+function getName(){
+    document.getElementById('name').innerText = movie.name;
+}
+function changeName(){
+    const newName = prompt('Input your name')
+    document.getElementById('name').innerText=newName;
+}
+getName()
+function getPrice(){
+    document.getElementById('price').innerText = movie.price;
+}
