634c03eb68cd2f7fba329aa535969095a9f65292
Luong-Cong-Duy97 <duylcong97@gmail.com>
2020-11-13 17:12:46 +0700

    Update

--- test.js
+++ test.js
@@ -2,25 +2,41 @@
 
 const movie={
     name:'Star War',
-    genre:'fiction',
+    genre:['fiction', 'action'],
     description:'This is movie about space war....',
-    actor:'luke skywalker',
+    actor:['luke skywalker','obiwan'],
     price:100000,
     duration: 100,
-    score:8,
-    releaseYear: 1997,
+    score: 8,
+    releaseYear: 1977,
     viewCount:420000,
 }
 console.log(string)
 
-function getName(){
+function getMovie(){
     document.getElementById('name').innerText = movie.name;
+    document.getElementById('genre').innerText = movie.genre;
+    document.getElementById('description').innerText = movie.description;
+    document.getElementById('actor').innerText = movie.actor;
+    document.getElementById('duration').innerText = movie.duration + " minutes";
+    document.getElementById('score').innerText = movie.score;
+    document.getElementById('releaseYear').innerText = movie.releaseYear;
+    document.getElementById('viewCount').innerText = movie.viewCount;
 }
 function changeName(){
     const newName = prompt('Input your name')
-    document.getElementById('name').innerText=newName;
+    document.getElementById('name').innerText= newName;
+    movie.name = newName;
 }
-getName()
-function getPrice(){
-    document.getElementById('price').innerText = movie.price;
+function voteScore()
+{   
+    
+    let NewScore = prompt('Input your score')
+    if( NewScore >=0 && NewScore <= 10 ){
+    let x = (Number(document.getElementById('score').innerText) + Number(NewScore))/2
+    document.getElementById('score').innerText = Math.round( x * 10 + Number.EPSILON ) / 10;
+    }
+    else {
+        alert("Please vote score from 0 to 10")
+    }
 }
